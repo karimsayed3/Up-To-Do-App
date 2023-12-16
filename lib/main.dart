429569e9fe0b19b710eacce98eb51c4c689b6a3c
karim sayed <76128546+karimsayed3@users.noreply.github.com>
2023-12-16 23:31:01 +0200

    remove some comments from main

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,5 @@ import 'package:uptodo/core/routing/app_router.dart';
 import 'package:uptodo/up_to_do_app.dart';
 
 void main() async{
-  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(UpToDoApp(appRouter: AppRouter(),));
 }
